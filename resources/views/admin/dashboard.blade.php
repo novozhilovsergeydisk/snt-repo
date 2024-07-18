02154d3867e590cb427aac9a09f5628b08dac00d
Novozhilov Sergey <novozhilovsergeydisk@yandex.ru>
2024-07-18 11:45:01 +0300

    modified:   resources/views/admin/dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -109,7 +109,7 @@
                 <!--@if ($depts)
                 <h4 class="text-muted text-ellipsis">@yield('welcome-j')</h4>-->
 
-		<h4>Статистика по состоянию на 1 июля 2023 г.</h4>
+		<h4>Статистика по состоянию на 15 июля 2024 г.</h4>
                 <hr>
                 <table class="table table-striped table-bordered table-responsive-sm">
                     <thead>
